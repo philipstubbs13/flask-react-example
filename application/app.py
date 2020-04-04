f0e7013003d3dd6f2a7ef72e628444560a04d7c5
philipstubbs13 <philipstubbs13@gmail.com>
2020-04-03 20:12:20 -0500

    deploy

--- application/app.py
+++
@@ -1,6 +0,0 @@
-from flask import request, render_template, jsonify, url_for, redirect, g
-from index import app
-
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
